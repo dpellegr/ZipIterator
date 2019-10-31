9283bc07b37065f6315078c75343601c71ce2b69
dpellegr <pellegrini.dario@gmail.com>
2019-10-31 16:05:21 +0100

    some improvements from code review

--- ZipIterator.hpp
+++ ZipIterator.hpp
@@ -3,41 +3,44 @@
 // Still unsure about the licence, but something in the line of just providing attribution
 // October 2019
 //
+// Include suggestions from https://codereview.stackexchange.com/questions/231352/c17-zip-iterator-compatible-with-stdsort
+//
 
+#include <cstddef>
 #include <tuple>
+#include <utility>
 
 template <typename ...T>
 class ZipRef {
   std::tuple<T*...> ptr;
+
+  template <std::size_t I = 0>
+  void copy_assign(const ZipRef& z) {
+    *(std::get<I>(ptr)) = *(std::get<I>(z.ptr));
+    if constexpr( I+1 < sizeof...(T) ) copy_assign<I+1>(z);
+  }
+  template <std::size_t I = 0>
+  void val_assign(const std::tuple<T...>& t) {
+    *(std::get<I>(ptr)) = std::get<I>(t);
+    if constexpr( I+1 < sizeof...(T) ) val_assign<I+1>(t);
+  }
+
 public:
   ZipRef() = delete;
   ZipRef(const ZipRef& z) = default;
   ZipRef(ZipRef&& z) = default;
   ZipRef(T* const... p): ptr(p...) {}
 
-  ZipRef& operator=(const ZipRef& z)             { return copy_assign( z); }
-  ZipRef& operator=(const std::tuple<T...>& val) { return val_assign(val); }
-
-  template <size_t I = 0>
-  ZipRef& copy_assign(const ZipRef& z) {
-    *(std::get<I>(ptr)) = *(std::get<I>(z.ptr));
-    if constexpr( I+1 < sizeof...(T) ) return copy_assign<I+1>(z);
-    return *this;
-  }
-  template <size_t I = 0>
-  ZipRef& val_assign(const std::tuple<T...>& t) {
-    *(std::get<I>(ptr)) = std::get<I>(t);
-    if constexpr( I+1 < sizeof...(T) ) return val_assign<I+1>(t);
-    return *this;
-  }
+  ZipRef& operator=(const ZipRef& z)             { copy_assign( z); return *this; }
+  ZipRef& operator=(const std::tuple<T...>& val) { val_assign(val); return *this; }
 
   std::tuple<T...> val() const {return std::apply([](auto&&...args){ return std::tuple((*args)...); }, ptr);}
   operator std::tuple<T...>() const { return val(); }
 
-  template <size_t I = 0>
-  void swap(const ZipRef& o) const {
-    std::swap(*(std::get<I>(ptr)), *(std::get<I>(o.ptr)));
-    if constexpr( I+1 < sizeof...(T) ) swap<I+1>(o);
+  template <std::size_t I = 0>
+  void swap_data(const ZipRef& o) const {
+    std::swap(*std::get<I>(ptr), *std::get<I>(o.ptr));
+    if constexpr(I+1 < sizeof...(T)) swap_data<I+1>(o);
   }
 
   #define OPERATOR(OP) \
@@ -48,23 +51,31 @@ public:
     OPERATOR(==) OPERATOR(<=) OPERATOR(>=)
     OPERATOR(!=) OPERATOR(<)  OPERATOR(>)
   #undef OPERATOR
-
 };
 
 template<typename ...IT>
 class ZipIter {
   std::tuple<IT...> it;
 
-  template<int N, typename... T> using NthTypeOf =
-    typename std::tuple_element<N, std::tuple<T...>>::type;
-  template<typename... T> using FirstTypeOf = NthTypeOf<0, T...>;
+  template <std::size_t I = 0>
+  bool one_is_equal(const ZipIter& rhs) const {
+    if (std::get<I>(it) == std::get<I>(rhs.it)) return true;
+    if constexpr(I+1 < sizeof...(IT)) return one_is_equal<I+1>(rhs);
+    return false;
+  }
+  template <std::size_t I = 0>
+  bool none_is_equal(const ZipIter& rhs) const {
+    if (std::get<I>(it) == std::get<I>(rhs.it)) return false;
+    if constexpr(I+1 < sizeof...(IT)) return none_is_equal<I+1>(rhs);
+    return true;
+  }
 
 public:
-  using iterator_category = typename std::iterator_traits<FirstTypeOf<IT...>>::iterator_category;
-  using difference_type   = typename std::iterator_traits<FirstTypeOf<IT...>>::difference_type;
+  using iterator_category = std::common_type_t<typename std::iterator_traits<IT>::iterator_category...>;
+  using difference_type   = std::common_type_t<typename std::iterator_traits<IT>::difference_type...>;
   using value_type        = std::tuple<typename std::iterator_traits<IT>::value_type ...>;
   using pointer           = std::tuple<typename std::iterator_traits<IT>::pointer ...>;
-  using reference         = ZipRef<typename std::iterator_traits<IT>::value_type ...>;
+  using reference         = ZipRef<std::remove_reference_t<typename std::iterator_traits<IT>::reference>...>;
 
   ZipIter() = default;
   ZipIter(const ZipIter &rhs) = default;
@@ -94,10 +105,14 @@ public:
   inline friend ZipIter operator+(const difference_type d, const ZipIter& z) {return z+d;}
   inline friend ZipIter operator-(const difference_type d, const ZipIter& z) {return z-d;}
 
+  // Since operator== and operator!= are often used to terminate cycles, 
+  // defining them as follow prevents incrementing behind the end() of a container
+  bool operator==(const ZipIter& rhs) const { return  one_is_equal(rhs); }
+  bool operator!=(const ZipIter& rhs) const { return none_is_equal(rhs); }
   #define OPERATOR(OP) \
     bool operator OP(const ZipIter& rhs) const {return it OP rhs.it;}
-    OPERATOR(==) OPERATOR(<=) OPERATOR(>=)
-    OPERATOR(!=) OPERATOR(<)  OPERATOR(>)
+    OPERATOR(<=) OPERATOR(>=)
+    OPERATOR(<)  OPERATOR(>)
   #undef OPERATOR
 };
 
@@ -112,18 +127,19 @@ public:
   Zip(Container&... z): zip(z...) {}
 
   #define HELPER(OP) \
-    auto OP(){return std::apply([](auto&&... args){ return ZipIter((args.OP())...);}, zip);}
+    auto OP(){return std::apply([](auto&&... args){ return ZipIter((args.OP())...);}, zip);} \
+    auto c##OP() const {return std::apply([](auto&&... args){ return ZipIter((args.c##OP())...);}, zip);}
+
     HELPER( begin) HELPER( end)
     HELPER(rbegin) HELPER(rend)
   #undef HELPER
 };
 
-#include <utility>
 using std::swap;
-template<typename ...T> void swap(const ZipRef<T...>& a, const ZipRef<T...>& b) { a.swap(b); }
+template<typename ...T> void swap(const ZipRef<T...>& a, const ZipRef<T...>& b) { a.swap_data(b); }
 
 #include <sstream>
-template< class Ch, class Tr, class...IT, typename std::enable_if<(sizeof...(IT)>0), int>::type = 0>
+template< class Ch, class Tr, class...IT, std::enable_if_t<(sizeof...(IT)>0), int> = 0>
 auto& operator<<(std::basic_ostream<Ch, Tr>& os, const ZipRef<IT...>& t) {
   std::basic_stringstream<Ch, Tr> ss;
   ss << "[ ";
